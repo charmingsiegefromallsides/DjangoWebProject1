5326bca5fae2c2e32b71da2d882ab9b90d0e3175
ren yakumo <charmingsiegefromallsides@gmail.com>
2022-12-31 00:27:47 -0700

    Add project files.

---
+++ DjangoWebProject1/app/static/app/scripts/customjs.js
@@ -0,0 +1,95 @@
+﻿var tag = 'nothing'
+
+function buttonClicked(buttonId) {
+    tag = buttonId;
+    console.log('in function button click');
+    console.log(tag);
+    // ...
+    tagHandoff(tag)
+}
+
+function tagHandoff(tag) {
+
+    var csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+
+    $.ajax({
+        type: "POST",
+        url: "geturl/",
+        headers: {
+            'X-CSRFToken': csrf_token
+        },
+        data: {
+            'tag': tag
+        },
+        success: function(data, tag) {
+            var url = data.urls;
+            console.log(url)
+            console.log(data)
+            $('#touhouimg').attr('src', url);
+            document.getElementById('touhouimg').src = url;
+            /*
+            var url = data.url;
+            console.log(url);
+            console.log(data.tag);
+            console.log("======");
+            cont = document.getElementById(data.tag);
+            document.getElementById(data.tag).innerHTML = '';
+            const img = document.createElement('img');
+            img.src = url;
+            cont.appendChild(img);
+            */
+        }
+    });
+}
+
+
+function update_image(url, tag) {
+    console.log(url)
+    console.log(tag)
+    cont = document.getElementById('tag')
+    document.getElementById('tag').innerHTML = ''
+    const img = document.createElement('img');
+    img.src = url;
+    cont.appendChild(img);
+}
+
+/*
+
+const remButton = document.getElementById('remilia_scarlet');
+const youButton = document.getElementById('konpaku_youmu');
+const suiButton = document.getElementById('ibuki_suika');
+const reiButton = document.getElementById('reisen_udongein_inaba');
+
+
+const cont = document.getElementById('image-container')
+
+remButton.addEventListener('click', () => {
+    document.getElementById('image-container').innerHTML = ''
+    const img = document.createElement('img');
+    img.src = 'https://safebooru.org/images/4064/c973350af0741c2e3a2fd82ba93152809ca5dbb9.png';
+    cont.appendChild(img);
+});
+
+youButton.addEventListener('click', () => {
+    document.getElementById('image-container').innerHTML = ''
+    const img = document.createElement('img');
+    img.src = 'https://safebooru.org/images/4060/d0fb6c1140231a0a74f55aceeb77f771a5b4757d.jpg';
+    cont.appendChild(img);
+});
+
+suiButton.addEventListener('click', () => {
+    document.getElementById('image-container').innerHTML = ''
+    const img = document.createElement('img');
+    img.src = 'https://safebooru.org/images/1942/f385430b6eeda653e5067b4237a292a9f2514df4.png';
+    cont.appendChild(img);
+});
+
+reiButton.addEventListener('click', () => {
+    document.getElementById('image-container').innerHTML = ''
+    const img = document.createElement('img');
+    img.src = 'https://safebooru.org//images/4066/fca63c2adeefad8d477a9670351e501422c783c0.jpg';
+    cont.appendChild(img);
+});
+
+
+*/
---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,87 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+import json
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.urls import path
+
+
+import urllib.request
+import xml.etree.ElementTree as ET
+import random
+from django.http import JsonResponse
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+
+  
+
+def geturl(request):
+    try:
+        touhouname=request.POST['tag']
+       
+        query_string='https://safebooru.org/index.php?page=dapi&s=post&q=index&tags='+touhouname+'&limit=1'
+        response = urllib.request.urlopen(query_string)
+        # Read the response
+        data = response.read()
+        root=ET.fromstring(data.decode('utf-8'))
+        max_=''
+        for x in root.attrib:
+            if x=='count':
+                max_=root.attrib[x]
+                break;
+        
+        rand_num=random.randint(0,int(max_))
+        query_string+="&pid="+str(rand_num)
+
+        response = urllib.request.urlopen(query_string)
+        data=response.read()
+        root=ET.fromstring(data.decode('utf-8'))
+        elem=root.find('./post')
+        #print(root.elem['post'].attrib['file_url'])
+        url=str(elem.attrib['file_url'])
+        print("generated url: "+url+" for touhou: " + touhouname)
+    except:
+        print("some error occuured")
+        return JsonResponse({'url': "ERROR"})
+    
+    return JsonResponse({'url': url})
